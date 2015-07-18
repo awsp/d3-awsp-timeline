9cb913bf3a81d67e3642598f8afe331f07a9779c
awsp <anthonyspwu@gmail.com>
2015-07-17 15:12:33 -1000

    change folder dest to dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var uglify = require('gulp-uglify');
 var typescript = require('gulp-tsc');
 var rename = require('gulp-rename');
 var Q = require('q');
+
 var outFileStem = 'd3-awsp.timeline';
+var out = 'dist/';
 
 // Tasks
 gulp.task('default', function () {
@@ -13,17 +15,17 @@ gulp.task('default', function () {
 gulp.task('build', ['compile-one', 'minify']);
 
 gulp.task('minify', ['compile-one'], function() {
-  return gulp.src('./dest/' + outFileStem + '.js')
+  return gulp.src('./' + out + outFileStem + '.js')
     .pipe(uglify())
     .pipe(rename(outFileStem + '.min.js'))
-    .pipe(gulp.dest('./dest/'))
+    .pipe(gulp.dest('./' + out))
   ;
 });
 
 gulp.task('compile', function () {
   return gulp.src(['./*.ts'])
     .pipe(typescript())
-    .pipe(gulp.dest('dest/'))
+    .pipe(gulp.dest(out))
   ;
 });
 
@@ -34,7 +36,7 @@ gulp.task('compile-one', function () {
     .pipe(typescript({
       out: outFileStem + '.js'
     }))
-    .pipe(gulp.dest('dest/'))
+    .pipe(gulp.dest(out))
     .on('end', function () {
       deferred.resolve();
     });
