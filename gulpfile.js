ad207863e9b5eee5ed7409ff89f2338b0ba885ed
awsp <anthonyspwu@gmail.com>
2015-07-23 15:57:29 -1000

    fix issue #10

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var typescript = require('gulp-tsc');
 var rename = require('gulp-rename');
@@ -12,31 +13,55 @@ gulp.task('default', function () {
 
 });
 
-gulp.task('build', ['compile-one', 'minify']);
+gulp.task('build', ['compile-one', 'minify-js', 'minify-css']);
 
-gulp.task('minify', ['compile-one'], function() {
-  return gulp.src('./' + out + outFileStem + '.js')
-    .pipe(uglify())
-    .pipe(rename(outFileStem + '.min.js'))
+/**
+ * Minify CSS Task
+ * - Exclude all *.min.css files.
+ */
+gulp.task('minify-css', function () {
+  return gulp.src([
+    './build/*.css',
+    '!build/*.min.css'
+  ]).pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./' + out))
-  ;
+    ;
 });
 
+/**
+ * Minify JS Task
+ * - Exclude all *.min.js files.
+ */
+gulp.task('minify-js', ['compile-one'], function () {
+  return gulp.src([
+    './' + out + '*.js',
+    '!' + out + '*.min.js',
+  ]).pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./' + out))
+    ;
+});
+
+/**
+ * Compile TypeScript Task
+ */
 gulp.task('compile', function () {
   return gulp.src(['./*.ts'])
     .pipe(typescript())
     .pipe(gulp.dest(out))
-  ;
+    ;
 });
 
+/**
+ * Compile TypeScript Task into one file.
+ */
 gulp.task('compile-one', function () {
   var deferred = Q.defer();
 
   gulp.src(['./*.ts'])
     .pipe(typescript({
       out: outFileStem + '.js',
-      //isolatedCompilation: true,
-      //noExternalResolve: true,
       noEmitOnError: false
     }))
     .pipe(gulp.dest(out))
